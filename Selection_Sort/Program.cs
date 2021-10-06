7a514b8f807e9d44487b6f1bbea088ba77bbefc8
CiMorgan <camorgan70@gmail.com>
2021-10-05 20:05:27 -0400

    Selection Sort

--- Selection_Sort/Program.cs
+++ Selection_Sort/Program.cs
@@ -4,9 +4,34 @@ namespace Selection_Sort
 {
     class Program
     {
+        static int[] selectionSort(int[] arr)
+        {
+            int len = arr.Length;
+            for(int i=0; i<len; i++)
+            {
+                int minIndex = i;
+                for(int j=i; j<len; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+
+            }
+            foreach (int x in arr)
+            {
+                Console.WriteLine(x);
+            }
+            return arr;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new int[] { 2, 7, 1, 3, 8, 11 };
+            selectionSort(arr);
         }
     }
 }
