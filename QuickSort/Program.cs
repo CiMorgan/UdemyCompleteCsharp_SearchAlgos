e0ada98c11e9fbeeeb7bff4da3ff6ce78a3ddd37
CiMorgan <camorgan70@gmail.com>
2021-11-01 19:36:14 -0400

    Quick Sort

---
+++ QuickSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while(true)
+            {
+                while(arr[left] < pivot)
+                {
+                    left++;
+                }
+                while(arr[right] > pivot)
+                {
+                    right--;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
