3bc303f419b462da9d96358fa1d37251e2252f7d
CiMorgan <camorgan70@gmail.com>
2021-11-01 20:09:10 -0400

    Quick Sort

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -4,10 +4,21 @@ namespace QuickSort
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 9, -3, 5, 2, 6, -8, -6, 1, 3 };
+            quickSort(arr, 1, 2);
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("Done");
+        }
         static int Partition(int[] arr, int left, int right)
         {
             int pivot = arr[left];
-            while(true)
+            
+            while (true)
             {
                 while(arr[left] < pivot)
                 {
@@ -15,13 +26,37 @@ namespace QuickSort
                 }
                 while(arr[right] > pivot)
                 {
+                    Console.WriteLine("Decrease right");
                     right--;
                 }
+                if (left < right)
+                {
+                    if(arr[left] == arr[right])
+                    {
+                        Console.WriteLine("inner if");
+                        return right;
+                    }
+                }
+
             }
         }
-        static void Main(string[] args)
+
+        static int[] quickSort(int[] arr, int left, int right)
         {
-            Console.WriteLine("Hello World!");
+            if(left < right)
+            {
+                int pivot = Partition(arr, left, right);
+                if(pivot > 1)
+                {
+                    quickSort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    quickSort(arr, pivot + 1, right);
+                }
+            }
+            return arr;
         }
+
     }
 }
