c3d410178bea5523482d2857469145b7b07b92a9
CiMorgan <camorgan70@gmail.com>
2021-11-04 15:55:17 -0400

    Insertion Sort

--- InsertSort/Program.cs
+++ InsertSort/Program.cs
@@ -4,6 +4,28 @@ namespace InsertSort
 {
     class Program
     {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] { 9, -3, 5, 2, 6, -8, -6, 1, 3 };
+            int[] x = insertionSort(arr);
+            foreach (int i in x)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        static int[] insertionSort(int[] arr)
+        {
+            for (int i=0; i<arr.Length; i++)
+            {
+                int value = arr[i];
+                for (int j = i-1; j>-1 && arr[j] > value; j--)
+                {
+                    arr[j + 1] = arr[j];
+                    arr[j] = value;
+                }
+            }
 
+            return arr;
+        }
     }
 }
