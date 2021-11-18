fc643a68c977300c4c72d978895921d3f950e6e3
CiMorgan <camorgan70@gmail.com>
2021-11-18 17:19:50 -0500

    Shell Sort

--- ShellSort/Program.cs
+++ ShellSort/Program.cs
@@ -8,5 +8,24 @@ namespace ShellSort
         {
             Console.WriteLine("Hello World!");
         }
+
+        int[] shellSort(int[] arr)
+        {
+            int length = arr.Length;
+            for (int gap=length/2; gap>0; gap/=2)
+            {
+                for (int i=gap; i<length; i+=1)
+                {
+                    int temp = arr[i];
+                    int j;
+                    for(j=1; j>=gap && arr[j-gap]>temp[]; j-=gap)
+                    {
+                        arr[j] = arr[j - gap];
+                    }
+                    arr[j] = temp;
+                }
+            }
+            return arr;
+        }
     }
 }
