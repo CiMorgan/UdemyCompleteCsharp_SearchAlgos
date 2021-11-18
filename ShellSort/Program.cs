13dfe3b7a0f4f8b6fac19b51f21e492a0245ba1e
CiMorgan <camorgan70@gmail.com>
2021-11-18 17:58:58 -0500

    Shell Sort Method 2

--- ShellSort/Program.cs
+++ ShellSort/Program.cs
@@ -6,31 +6,55 @@ namespace ShellSort
     {
         static void Main(string[] args)
         {
-            int[] arrOrg = new int[] { 4, 8, 1, 0, 2, 7, 11, 6 };
+            int[] arrOrg = new int[] { 4, 8, -1, 0, 2, 7, 11, 6 };
             shellSort(arrOrg);
 
 
         }
 
+        //static int[] shellSort(int[] arr)
+        //{
+        //    int length = arr.Length;
+        //    int gap = length/2;
+        //    int temp, i, j;
+        //    while (gap > 0)
+        //    {
+        //        for (i=gap; i<length; i++)
+        //        {
+        //            temp = arr[i];
+        //            j=i;
+        //            while(j >= gap && arr[j-gap]>temp)
+        //            {
+        //                arr[j] = arr[j - gap];
+        //                j = j - gap;
+        //            }
+        //            arr[j] = temp;
+        //        }
+        //        gap = gap / 2;
+        //    }
+        //    foreach (int a in arr)
+        //    {
+        //        Console.Write(a + " ");
+        //    }
+        //    return arr;
+        //}
+
         static int[] shellSort(int[] arr)
         {
             int length = arr.Length;
-            int gap = length/2;
-            int temp, i, j;
-            while (gap > 0)
+ 
+            for (int gap = length / 2; gap > 0; gap /= 2)
             {
-                for (i=gap; i<length; i++)
+                for (int i = gap; i < length; i++)
                 {
-                    temp = arr[i];
-                    j=i;
-                    while(j >= gap && arr[j-gap]>temp)
+                    int temp = arr[i];
+                    int j;
+                    for (j=i;  j >= gap && arr[j - gap] > temp; j-=gap)
                     {
                         arr[j] = arr[j - gap];
-                        j = j - gap;
                     }
                     arr[j] = temp;
                 }
-                gap = gap / 2;
             }
             foreach (int a in arr)
             {
